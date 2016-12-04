76c6644bd4abe18c2d34f603510663e28aa43afd
MBulli <markusbullmann@gmail.com>
2016-12-04 13:24:36 +0100

    Added threshold value for pesudo inverse

--- CurveFit/CurveFit/Extensions.cs
+++ CurveFit/CurveFit/Extensions.cs
@@ -9,20 +9,16 @@ namespace CurveFit
 {
     static class Extensions
     {
-        public static Matrix<double> PseudoInverse(this Matrix<double> m)
+        public static Matrix<double> PseudoInverse(this Matrix<double> m, double eps = 1e-5)
         {
-            double eps = 0;
-
             var svd = m.Svd();
             var V = svd.VT.Transpose();
-            var U = svd.U;
-            var S = svd.W;
 
             var Sigma = Matrix<double>.Build.DenseOfDiagonalVector(rows: V.ColumnCount,
-                                                                   columns: U.RowCount,
-                                                                   diagonal: svd.S.PointwisePower(-1));
+                                                                   columns: svd.U.RowCount,
+                                                                   diagonal: svd.S.Map(s => (s >= eps) ? (1.0 / s) : 0));
 
-            return V * Sigma * U.Transpose();
+            return V * Sigma * svd.U.Transpose();
         }
     }
 }
